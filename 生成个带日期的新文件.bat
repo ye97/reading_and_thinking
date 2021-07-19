a6a01704721edb8ce168d0c998f17114e7c93463
swu ye <ye79_2020@qq.com>
2021-07-19 21:32:12 +0800

    2021/07/19-21：31：46，关机

--- 生成个带日期的新文件.bat
+++ 生成个带日期的新文件.bat
@@ -4,3 +4,8 @@ rem 下面是复制一份
 rem  copy %1 "%~n1-%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%-%time:~3,2%-%time:~6,2%%~x1"
 
 copy  %1 "%~n1-%date:~0,4%-%date:~5,2%-%date:~8,2%%~x1"
+
+set filename="%~n1-%date:~0,4%-%date:~5,2%-%date:~8,2%%~x1"
+
+md     %filename%
+
---
+++ 生活总结/dairy/dairy.md
@@ -0,0 +1,13 @@
+---
+title: dairy
+categories:
+  - 总结
+description: <read more ...>
+mathjax: true
+permalink_defaults: 'category/:title/'
+date: 2021-07-19 20:49:19
+urlname:
+tags:
+---
+
+![](dairy/20190905165255848.png)
